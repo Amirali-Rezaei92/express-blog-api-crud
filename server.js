1d5c70915c66b13bce0e38655ea9aab4f28520cd
Amirali Rezaei <amiralirezaie169@gmail.com>
2026-05-27 22:54:14 +0200

    feat: add 404 handler and global error middleware (Milestone 1)

--- server.js
+++ server.js
@@ -7,6 +7,22 @@ app.use(express.json());
 
 app.use("/posts", postsRouter);
 
+app.use((req, res, next) => {
+    res.status(404).json({
+        error: "Endpoint non trovato",
+        path: req.originalUrl
+    });
+});
+app.use((err, req, res, next) => {
+    console.error("🔥 ERRORE:", err);
+
+    res.status(err.status || 500).json({
+        error: err.message || "Errore interno del server"
+    });
+});
+
+
+
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
